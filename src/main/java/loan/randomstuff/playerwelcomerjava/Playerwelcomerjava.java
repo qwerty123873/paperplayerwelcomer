5dbe563269b327039be8269a019334c80a7f26c6
Brandon <brandonding21@gmail.com>
2025-08-09 23:59:35 -0400

    dosifjsdoj

--- src/main/java/loan/randomstuff/playerwelcomerjava/Playerwelcomerjava.java
+++ src/main/java/loan/randomstuff/playerwelcomerjava/Playerwelcomerjava.java
@@ -1,17 +1,24 @@
 package loan.randomstuff.playerwelcomerjava;
 
+import net.kyori.adventure.text.Component;
+import org.bukkit.event.EventHandler;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerEvent;
+import org.bukkit.event.Listener;
 
-public final class Playerwelcomerjava extends JavaPlugin {
+public final class Playerwelcomerjava extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        getServer().getPluginManager().registerEvents(this, this);
+        getLogger().info("Plugin Loaded!");
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        event.getPlayer().sendMessage(Component.text(event.getPlayer().getName() + " has joined."));
+
     }
 }
