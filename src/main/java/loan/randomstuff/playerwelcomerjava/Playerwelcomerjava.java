cbc4d780d397926e28bffb7cd31e38d8e359f795
Brandon <106548043+qwerty123873@users.noreply.github.com>
2025-08-20 23:41:52 -0400

    added config file to specify server name

--- src/main/java/loan/randomstuff/playerwelcomerjava/Playerwelcomerjava.java
+++ src/main/java/loan/randomstuff/playerwelcomerjava/Playerwelcomerjava.java
@@ -5,6 +5,7 @@ import org.bukkit.Bukkit;
 import net.kyori.adventure.text.format.TextColor;
 import net.kyori.adventure.text.minimessage.*;
 import net.kyori.adventure.text.Component;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.EventHandler;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -14,21 +15,27 @@ import org.bukkit.event.player.PlayerEvent;
 import org.bukkit.event.Listener;
 
 public final class Playerwelcomerjava extends JavaPlugin implements Listener {
-
     @Override
     public void onEnable() {
         // Plugin startup logic
         getServer().getPluginManager().registerEvents(this, this);
+        saveResource("config.yml", /* replace */ false);
+        FileConfiguration config = this.getConfig();
+        String serverName = config.getString("server_name");
+        getLogger().info("You can set the server name by changing the server_name value in playerwelcomerjava/config.yml");
+        getLogger().info("Currently set name: " + serverName);
         getLogger().info("Plugin Loaded!");
     }
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         event.joinMessage(null);
-        Component playerWelcome = Component.text("You're connected to the ")
+        FileConfiguration config = this.getConfig();
+        String serverName = config.getString("server_name");
+        Component playerWelcome = Component.text("You're connected to ")
                 .color(TextColor.color(63, 176, 120))
-                .append(Component.text("manhunt ", NamedTextColor.AQUA))
-                .append(Component.text("server."))
+                .append(Component.text(serverName, NamedTextColor.AQUA))
+                .append(Component.text("."))
                 .color(TextColor.color(63, 176, 120));
         event.getPlayer().sendMessage(playerWelcome);
         Component joinBroadcast = Component.text(event.getPlayer().getName() + " has connected to the server!")
